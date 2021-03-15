d3b0003179583fef94db70335c8a2ce4740e4ffe
Daniel Z. Huang <hzyhzy2014@gmail.com>
2021-03-15 00:13:28 -0700

    update nn train

---
+++ nn/Box_N2D_nn_Data.jl
@@ -0,0 +1,130 @@
+using Random, Distributions, NPZ
+include("Box_Neumann_To_Dirichlet.jl")
+
+
+function c_func_uniform(θ::Float64) 
+    
+    c = 50 + θ 
+    
+    return c
+end
+
+
+
+#=
+Compute sorted pair (i, j), sorted by i^2 + j^2
+wInt64h i≥0 and j≥0 and i+j>0
+
+These pairs are used for Karhunen–Loève expansion
+=#
+function compute_seq_pairs(N_KL::Int64) 
+    seq_pairs = zeros(Int64, N_KL, 2)
+    trunc_Nx = trunc(Int64, sqrt(2*N_KL)) + 1
+    
+    seq_pairs = zeros(Int64, (trunc_Nx+1)^2 - 1, 2)
+    seq_pairs_mag = zeros(Int64, (trunc_Nx+1)^2 - 1)
+    
+    seq_pairs_i = 0
+    for i = 0:trunc_Nx
+        for j = 0:trunc_Nx
+            if (i == 0 && j ==0)
+                continue
+            end
+            seq_pairs_i += 1
+            seq_pairs[seq_pairs_i, :] .= i, j
+            seq_pairs_mag[seq_pairs_i] = i^2 + j^2
+        end
+    end
+    
+    seq_pairs = seq_pairs[sortperm(seq_pairs_mag), :]
+    return seq_pairs[1:N_KL, :]
+end
+
+
+#=
+Generate parameters for logk field, based on Karhunen–Loève expansion.
+They include eigenfunctions φ, eigenvalues λ and the reference parameters θ_ref, 
+and reference field logk_2d field
+
+logκ = ∑ u_l √λ_l φ_l(x)                l = (l₁,l₂) ∈ Z^{0+}×Z^{0+} \ (0,0)
+
+where φ_{l}(x) = √2 cos(πl₁x₁)             l₂ = 0
+                 √2 cos(πl₂x₂)             l₁ = 0
+                 2  cos(πl₁x₁)cos(πl₂x₂) 
+      λ_{l} = (π^2l^2 + τ^2)^{-d} 
+
+They can be sorted, where the eigenvalues λ_{l} are in descending order
+
+generate_θ_KL function generates the summation of the first N_KL terms 
+=#
+function c_func_random(x1::Float64, x2::Float64, θ::Array{Float64, 1}, seq_pairs::Array{Float64, 2}, d::Float64=2.0, τ::Float64=3.0) 
+    
+    N_KL = length(θ)
+    
+    a = 0
+    
+    for i = 1:N_KL
+        λ = (pi^2*(seq_pairs[i, 1]^2 + seq_pairs[i, 2]^2) + τ^2)^(-d)
+        
+        if (seq_pairs[i, 1] == 0 && seq_pairs[i, 2] == 0)
+            a += θ[i] * λ
+        elseif (seq_pairs[i, 1] == 0)
+            a += θ[i] * λ * sqrt(2)*cos.(pi * (seq_pairs[i, 2]*x2))
+        elseif (seq_pairs[i, 2] == 0)
+            a += θ[i] * λ * sqrt(2)*cos.(pi * (seq_pairs[i, 1]*x1))
+        else
+            a += θ[i] * λ * 2*cos.(pi * (seq_pairs[i, 1]*x1)) .*  cos.(pi * (seq_pairs[i, 2]*x2))
+        end
+
+        
+    end
+    
+    c = 50 + exp(a)
+    
+    return c
+end
+
+
+
+function Data_Generate(generate_method::String, data_type::String, N_θ::Int64, N_per_θ::Int64; 
+    ne::Int64 = 100,   seed::Int64=123)
+    @assert(generate_method == "Uniform" || generate_method == "Random")
+    @assert(data_type == "Direct" || generate_method == "Indirect")
+    
+    porder = 1
+    Δx = 1.0/ne
+    K_scale = zeros(Float64, ne*porder+1) .+ Δx
+    K_scale[1] = K_scale[end] = Δx/2.0
+    Random.seed!(seed)
+
+    if generate_method == "Uniform" && data_type == "Direct"
+        
+        θ = rand(Uniform(0, 50), N_θ, 1);
+        κ = zeros(ne+1, ne+1, N_θ)
+        for i = 1:N_θ
+            cs = [(x,y)->c_func_uniform(θ[i]);]
+            # generate Dirichlet to Neumman results output for different condInt64ions
+            # data =[nodal posInt64ions, (x, ∂u∂n, u), 4 edges, experiment number]
+            data = Generate_Input_Output(cs, ne, porder);
+            
+            # data =[nodal posInt64ions, (x, ∂u∂n, u), 4 edges, experiment number]
+            bc_id = 3
+            u_n = data[:, 2, bc_id, :]
+            u_d = data[:, 3, bc_id, :]
+            K = u_d/u_n
+            κ[:, :, i] = K ./ K_scale' 
+        end 
+        
+        npzwrite("uniform_direct_theta.npy", θ)
+        npzwrite("uniform_direct_K.npy", κ)
+        
+    else 
+        @info "generate_method: $(generate_method) and data_type == $(data_type) have not implemented yet"
+    end
+    
+    
+    
+end
+
+
+Data_Generate("Uniform", "Direct", 10, 0; ne = 10,   seed = 123)
--- nn/Box_Neumann_To_Dirichlet.jl
+++ nn/Box_Neumann_To_Dirichlet.jl
@@ -36,8 +36,6 @@ function N2D(x0::Float64, d0::Float64, c_func::Function, nex::Int64 = 20, ney::I
     
     # define fourier boundary
     # f(x) = 1_{ |x - x0| < d0/2.0 } 
-    fl = (x,l)-> (l*π)^-2 * xi[l] * sin.(l*π*x)
-    
     bc_funcs = [(x,y)->0, (x,y)->0, (x,y)->bump_func(x, y, x0,d0), (x,y)->0]
 
     ω = 1000.0
@@ -85,7 +83,7 @@ function N2D(x0::Float64, d0::Float64, c_func::Function, nex::Int64 = 20, ney::I
 end
 
 
-function Generate_Input_Output(cs::Array{Float64,1}, x0d0s::Array{Float64, 2}, ne::Int64, porder::Int64)
+function Generate_Input_Output(func_cs, x0d0s::Array{Float64, 2}, ne::Int64, porder::Int64)
     # domain discretization set-up
     Lx, Ly = 1.0, 1.0   # box edge lengths
     nex, ney = ne, ne   # grid size
@@ -94,17 +92,17 @@ function Generate_Input_Output(cs::Array{Float64,1}, x0d0s::Array{Float64, 2}, n
     # number of Dirichlet boundary edges
     n_dbcs = 4
     n_points = nex*porder + 1
-    data_all = zeros(Float64, n_points,  3, n_dbcs, length(cs)*size(x0d0s, 1)) # nodal positions, (x, ∂u∂n, u), 4 edges, experiment number
+    data_all = zeros(Float64, n_points,  3, n_dbcs, length(func_cs)*size(x0d0s, 1)) # nodal positions, (x, ∂u∂n, u), 4 edges, experiment number
     # generate data
 
     
-    for ind_c = 1:length(cs)
-        c = cs[ind_c]
+    for ind_c = 1:length(func_cs)
+        func_c = func_cs[ind_c]
         
         for l = 1:size(x0d0s, 1)
             x0, d0 = x0d0s[l, :]
             
-            data = N2D(x0, d0, (x,y)->c, nex, ney, porder; visualize = (l == 1))
+            data = N2D(x0, d0, func_c, nex, ney, porder; visualize = (l == 1))
             for i = 1:n_dbcs
                 
                 data_all[:,  :,  :, (ind_c - 1)*N_l + l] .= data
@@ -115,4 +113,134 @@ function Generate_Input_Output(cs::Array{Float64,1}, x0d0s::Array{Float64, 2}, n
     return data_all
 end
 
+
+
+
+###################################################################################
+
+function hat_func(x, y, x0, Δx) 
+    if x >= x0 && x <= x0 + Δx
+        
+        return 1.0 - (x - x0)/Δx
+
+    elseif  x <= x0 && x >= x0 - Δx
+
+        return 1.0 - (x0 - x)/Δx
+
+    else 
+    
+        return 0.0
+    end
+end
+
+
+# Generate with hat function at the ih-th point, ih = 0, 1 ... nex
+function N2D(ih::Int64, c_func::Function, nex::Int64 = 20, ney::Int64 = 20, porder::Int64 = 2, ngp::Int64 = 3;
+    visualize::Bool = false )
+    """
+    -Δu - ω^2/c^2 u = 0  in Ω=[0,1]×[0,1]
+    
+    boundary condition orders:
+    
+    bottom N = (0, -1)  
+    right  N = (1,  0)  
+    top    N = (0,  1) 
+    left   N = (-1, 0)  
+    
+    """
+    
+    """
+    ----- 3 -----
+    |           |
+    |           |
+    4           2
+    |           |
+    |           |
+    ----- 1 -----
+    """
+    ω = 1000.0
+    s_func = (x,y)-> 0
+    
+    Lx, Ly = 1.0, 1.0
+
+    Δx = Lx / nex
+ 
+    bc_types = ["Neumann", "Neumann", "Neumann", "Neumann"]
+    
+    # define fourier boundary
+    # f(x) = 1_{ |x - x0| < d0/2.0 } 
+    bc_funcs = [(x,y)->0, (x,y)->0, (x,y)->hat_func(x, y, Δx*ih, Δx), (x,y)->0]
+
+
+    nodes, elnodes, bc_nodes = box(Lx, Ly, nex, ney, porder)
+    
+    domain = Domain(nodes, elnodes,
+    bc_nodes, bc_types, bc_funcs,
+    porder, ngp; 
+    ω = ω, 
+    c_func = c_func, 
+    s_func = s_func)
+    
+    domain = solve!(domain)
+
+    if visualize
+        PyPlot.figure()
+        PyPlot.title("hat func $(ih)")
+        visScalarField(domain, domain.state, savefile="hat_func_$(ih).png")
+        
+    end
+    
+    u_data = computeDirichletOnNeumannNode(domain, bc_nodes, bc_types)
+    
+    data = zeros(nex*porder+1, 3, 4) # nodal positions, (x, ∂u∂n, u), 4 edges
+    for bc_id = 1:4
+        bc_id_nodes = bc_nodes[:, bc_id]
+        xx, yy = nodes[bc_id_nodes, 1], nodes[bc_id_nodes, 2]
+        data_Neumann = [bc_funcs[bc_id](xx[i], yy[i]) for i =1:length(xx)]
+        data_Dirichlet = u_data[bc_id, bc_id_nodes]
+
+   
+        data[:, 1, bc_id] .= (bc_id == 1 || bc_id == 3) ? xx : yy
+        if bc_id == 3
+            for ip = 1: size(data, 1)
+                data[ip, 2, bc_id] = hat_func(data[ip, 1, bc_id], 0.0, Δx*ih, Δx)
+            end
+        end
+        data[:, 3, bc_id] .= data_Dirichlet
+        # sort the data
+        data[:, :, bc_id] .= data[sortperm(data[:, 1, bc_id]), :, bc_id]
+    end 
+
+    return data
+end
+
+
+function Generate_Input_Output(func_cs, ne::Int64, porder::Int64)
+
+    @assert(porder == 1)
+    # domain discretization set-up
+    Lx, Ly = 1.0, 1.0   # box edge lengths
+    nex, ney = ne, ne   # grid size
+    
+    # number of Dirichlet boundary edges
+    n_dbcs = 4
+    n_points = nex*porder + 1
+    data_all = zeros(Float64, n_points,  3, n_dbcs, length(func_cs)*n_points) # nodal positions, (x, ∂u∂n, u), 4 edges, experiment number
+    # generate data
+
+    
+    for ind_c = 1:length(func_cs)
+        func_c = func_cs[ind_c]
+        
+        for l = 1:n_points
+            data = N2D(l - 1, func_c, nex, ney, porder; visualize = (l == 1))
+            for i = 1:n_dbcs
+                
+                data_all[:,  :,  :, (ind_c - 1)*n_points + l] .= data
+            end
+        end
+    end
+
+    return data_all
+end
 

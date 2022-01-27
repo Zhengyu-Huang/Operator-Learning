bbf8321983c57c9b0e3d289fdd23cba156999f9b
Elizabeth Qian <elizqian@mit.edu>
2022-01-27 13:29:17 -0500

    plus plotting defaults

---
+++ plotdefaults.jl
@@ -0,0 +1,18 @@
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+    mysize = 22
+    font0 = Dict(
+    "font.size" => 30,          # title
+    "axes.labelsize" => 26, # axes labels
+    "xtick.labelsize" => mysize,
+    "ytick.labelsize" => mysize,
+    "legend.fontsize" => mysize,
+    "lines.linewidth" => 3,
+    "lines.markersize" =>10,
+    )
+merge!(rcParams, font0)
+
+nns = ["PCA-Net", "DeepO-Net", "PARA-Net", "FNO"]
+sizes = [L"w = 16\,\,/\,\,d_f = 2",L"w = 64 \,\,/\,\ d_f = 4",L"w = 128 \,\,/\,\ d_f = 8",L"w = 256 \,\,/\,\ d_f = 16"]
+colors = ["#3A637B", "#C4A46B", "#FF6917", "#D44141" ] # colorblind friendly pallet https://davidmathlogic.com/colorblind/#%233A637B-%23C4A46B-%23FF6917-%23D44141
+markers = ["o", "s", "^", "*"]
+linestyle = ["dotted", "-.", "--", "-", ]

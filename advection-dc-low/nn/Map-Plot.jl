311a414b04cb7f5165dfc47ede9b25b7db53b763
Daniel-Huang <dzhuang@caltech.edu>
2022-02-14 14:48:51 -0800

    add num of param in mynn.jl

--- advection-dc-low/nn/Map-Plot.jl
+++ advection-dc-low/nn/Map-Plot.jl
@@ -61,7 +61,7 @@ function output_plot(data, file_name)
     fig.savefig(file_name)
 end
 
-function map_plot(prefix = "../../data/", inds = [1,11])
+function map_plot(prefix = "../../data/", inds = [18,28])
     inputs   = npzread(prefix * "adv_a0.npy")   
     outputs  = npzread(prefix * "adv_aT.npy")
     
@@ -134,80 +134,80 @@ map_plot("../src/")
 # prediction_plot("DeepONet", 10000, 128)
 # prediction_plot("FNO", 10000, 16)
 
-ntrain = 10000
-widths = [128, 128, 128, 16]
-ind = 2 # median error
-fig, ax = PyPlot.subplots(3,4, sharex=true, sharey=true, figsize=(6.5,3.25))
-for i = 1:4
-    nn_name = nn_names[i]
-    inputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_input_save.npy"
-    outputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_output_save.npy"
-    inputs   = npzread(inputfile)   
-    outputs  = npzread(outputfile)
+# ntrain = 10000
+# widths = [128, 128, 128, 16]
+# ind = 2 # median error
+# fig, ax = PyPlot.subplots(3,4, sharex=true, sharey=true, figsize=(6.5,3.25))
+# for i = 1:4
+#     nn_name = nn_names[i]
+#     inputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_input_save.npy"
+#     outputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_output_save.npy"
+#     inputs   = npzread(inputfile)   
+#     outputs  = npzread(outputfile)
     
-    N_x, _ = size(inputs)
-    L = 1
-    xx = LinRange(0, L, N_x)
+#     N_x, _ = size(inputs)
+#     L = 1
+#     xx = LinRange(0, L, N_x)
 
-    ax[1,i].plot(xx, inputs[:, ind], "--o", fillstyle="none", color="#a1a1a1")
-    ax[2,i].plot(xx, outputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
-    ax[3,i].plot(xx, outputs[:, ind+3], "--o", fillstyle="none",color=colors[i])
+#     ax[1,i].plot(xx, inputs[:, ind], "--o", fillstyle="none", color="#a1a1a1")
+#     ax[2,i].plot(xx, outputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
+#     ax[3,i].plot(xx, outputs[:, ind+3], "--o", fillstyle="none",color=colors[i])
 
-    ax[1,i].set_title(nns[i],pad = 2)
-    ax[3,i].set_xlabel(L"x",labelpad=1)
+#     ax[1,i].set_title(nns[i],pad = 2)
+#     ax[3,i].set_xlabel(L"x",labelpad=1)
 
-    for j = 1:3
-        ax[j,i].spines["top"].set_visible(false)
-        ax[j,i].spines["right"].set_visible(false)
-        ax[j,i].spines["left"].set_color("#808080")
-        ax[j,i].spines["left"].set_linewidth(0.3)
-        ax[j,i].spines["bottom"].set_color("#808080")
-        ax[j,i].spines["bottom"].set_linewidth(0.3)
-        ax[j,i][:xaxis][:set_tick_params](colors="#808080",width=0.3)
-        ax[j,i][:yaxis][:set_tick_params](colors="#808080",width=0.3)
-    end
-end
-ax[1,1].set_ylabel(L"a_0")
-ax[2,1].set_ylabel("True "*L"a(T)")
-ax[3,1].set_ylabel("Predicted "*L"a(T)")
-plt.subplots_adjust(left = 0.08, right = 0.98, bottom = 0.1,top=.9,hspace=0.1,wspace=0.1)
-plt.savefig("Advection-dc-low-medians.pdf")
+#     for j = 1:3
+#         ax[j,i].spines["top"].set_visible(false)
+#         ax[j,i].spines["right"].set_visible(false)
+#         ax[j,i].spines["left"].set_color("#808080")
+#         ax[j,i].spines["left"].set_linewidth(0.3)
+#         ax[j,i].spines["bottom"].set_color("#808080")
+#         ax[j,i].spines["bottom"].set_linewidth(0.3)
+#         ax[j,i][:xaxis][:set_tick_params](colors="#808080",width=0.3)
+#         ax[j,i][:yaxis][:set_tick_params](colors="#808080",width=0.3)
+#     end
+# end
+# ax[1,1].set_ylabel(L"a_0")
+# ax[2,1].set_ylabel("True "*L"a(T)")
+# ax[3,1].set_ylabel("Predicted "*L"a(T)")
+# plt.subplots_adjust(left = 0.08, right = 0.98, bottom = 0.1,top=.9,hspace=0.1,wspace=0.1)
+# plt.savefig("Advection-dc-low-medians.pdf")
 
-ind = 3 # largest error
-fig, ax = PyPlot.subplots(3,4, sharex=true, sharey=true, figsize=(6.5,3.25))
-for i = 1:4
-    nn_name = nn_names[i]
-    inputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_input_save.npy"
-    outputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_output_save.npy"
-    inputs   = npzread(inputfile)   
-    outputs  = npzread(outputfile)
+# ind = 3 # largest error
+# fig, ax = PyPlot.subplots(3,4, sharex=true, sharey=true, figsize=(6.5,3.25))
+# for i = 1:4
+#     nn_name = nn_names[i]
+#     inputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_input_save.npy"
+#     outputfile = nn_name * "/" * string(ntrain) * "_" * string(widths[i]) * "_test_output_save.npy"
+#     inputs   = npzread(inputfile)   
+#     outputs  = npzread(outputfile)
     
-    N_x, _ = size(inputs)
-    L = 1
-    xx = LinRange(0, L, N_x)
+#     N_x, _ = size(inputs)
+#     L = 1
+#     xx = LinRange(0, L, N_x)
 
-    ax[1,i].plot(xx, inputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
-    ax[2,i].plot(xx, outputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
-    ax[3,i].plot(xx, outputs[:, ind+3], "--o", fillstyle="none", color=colors[i],clip_on=false)
+#     ax[1,i].plot(xx, inputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
+#     ax[2,i].plot(xx, outputs[:, ind], "--o", fillstyle="none",color="#a1a1a1")
+#     ax[3,i].plot(xx, outputs[:, ind+3], "--o", fillstyle="none", color=colors[i],clip_on=false)
 
-    ax[1,i].set_title(nns[i],pad = 2)
-    ax[3,i].set_xlabel(L"x",labelpad=1)
+#     ax[1,i].set_title(nns[i],pad = 2)
+#     ax[3,i].set_xlabel(L"x",labelpad=1)
 
-    for j = 1:3
-        ax[j,i].spines["top"].set_visible(false)
-        ax[j,i].spines["right"].set_visible(false)
-        ax[j,i].spines["left"].set_color("#808080")
-        ax[j,i].spines["left"].set_linewidth(0.3)
-        ax[j,i].spines["bottom"].set_color("#808080")
-        ax[j,i].spines["bottom"].set_linewidth(0.3)
-        ax[j,i][:xaxis][:set_tick_params](colors="#808080",width=0.3)
-        ax[j,i][:yaxis][:set_tick_params](colors="#808080",width=0.3)
-        ax[j,i].set_ylim([-1.3,1.3])
-    end
-end
-ax[1,1].set_ylabel(L"a_0")
-ax[2,1].set_ylabel("True "*L"a(T)")
-ax[3,1].set_ylabel("Predicted "*L"a(T)")
-# plt.tight_layout()
-plt.subplots_adjust(left = 0.08, right = 0.98, bottom = 0.1,top=.9,hspace=0.1,wspace=0.1)
-plt.savefig("Advection-dc-low-worst.pdf")
+#     for j = 1:3
+#         ax[j,i].spines["top"].set_visible(false)
+#         ax[j,i].spines["right"].set_visible(false)
+#         ax[j,i].spines["left"].set_color("#808080")
+#         ax[j,i].spines["left"].set_linewidth(0.3)
+#         ax[j,i].spines["bottom"].set_color("#808080")
+#         ax[j,i].spines["bottom"].set_linewidth(0.3)
+#         ax[j,i][:xaxis][:set_tick_params](colors="#808080",width=0.3)
+#         ax[j,i][:yaxis][:set_tick_params](colors="#808080",width=0.3)
+#         ax[j,i].set_ylim([-1.3,1.3])
+#     end
+# end
+# ax[1,1].set_ylabel(L"a_0")
+# ax[2,1].set_ylabel("True "*L"a(T)")
+# ax[3,1].set_ylabel("Predicted "*L"a(T)")
+# # plt.tight_layout()
+# plt.subplots_adjust(left = 0.08, right = 0.98, bottom = 0.1,top=.9,hspace=0.1,wspace=0.1)
+# plt.savefig("Advection-dc-low-worst.pdf")
--- nn/mynn.jl
+++ nn/mynn.jl
@@ -28,5 +28,64 @@ end
 
 function FNO_Net_Cost(df, k, layers, Np)
     #                 fourier fft/ifft        multiply    pointwise 
-    return Np*df + layers*(  df*(2*5*Np*log(Np) + 2*k*k - k + Np) + Np*(2*df*df - df)  )  +     Np*(2df-1)
+    return Np*df + layers*(  df*(2*5*Np*log(Np) + 2*k*k - k + Np) + Np*(2*df*df)  )  +     Np*(2df-1)
+end
+
+
+
+
+
+
+
+
+function FNN_Para_Num(ns)
+    layers = length(ns) - 1
+    c = 0
+    for i = 1:layers
+        c += ns[i]*ns[i+1] + ns[i+1]
+    end
+    return c
+end
+function PCA_Para_Num(n_in, width, layers)
+    ns = pushfirst!([width for i = 1:layers], n_in)
+    c = FNN_Cost(ns)
+    return c
+end
+
+function PARA_Para_Num(n_in, n_y, width, layers)
+    ns = push!(pushfirst!([width for i = 1:layers-1], n_in+n_y), 1)
+    c = FNN_Cost(ns)
+    return c
+end
+
+function DeepO_Para_Num(n_in, n_y, width, layers)
+    ns_branch = pushfirst!([width for i = 1:layers], n_in)
+    ns_trunk = pushfirst!([width for i = 1:layers], n_y)
+    c = FNN_Cost(ns_branch) + FNN_Cost(ns_trunk)
+    return c
+end
+
+function FNO_Para_Num(df, k, layers)
+    #                 fourier fft/ifft        multiply    pointwise 
+    d_i = d_o = 1
+    return df*d_i + df + df*d_o + d_o + layers*(df^2 + df + df*k^2)
+end
+
+
+function compute_Para_Num()
+    # NS
+    layers = 4
+    n_in = 128
+    n_y = 2
+    k = 12^2
+    for width in [16,64,128,256,512]
+        @info "PCA-Net : ", PCA_Para_Num(n_in, width, layers)
+        @info "DeepO-Net : ", DeepO_Para_Num(n_in, n_y, width, layers)
+        @info "PARA-Net : ", PARA_Para_Num(n_in, n_y, width, layers)
+    end
+
+
+    for df in [2,4,8,16,32]
+        @info "FNO : ", FNO_Para_Num(df, k, layers)
+    end
 end

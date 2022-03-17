0c13cc41d1065b52f49113fbd8a45a370c04fbd5
Elizabeth Qian <eqian@caltech.edu>
2022-03-16 17:18:30 -0700

    updated plotting scripts

---
+++ plot-err-v-cost.jl
@@ -0,0 +1,67 @@
+
+
+
+########### Error vs Cost
+fig, ax = PyPlot.subplots(ncols = 4,nrows=4, sharex="col", sharey= "row", figsize=(6.8,5.5))
+N_Data = [2500, 5000, 10000, 20000]
+ypad = [1, 1, 2,7]
+
+for i = 1:4 # test problems
+    for j = 1:4 # plot columns
+
+        ax[i,j].loglog(PCA_Data[(j+3)*5+1:(j+3)*5+5, 3, i], PCA_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[1], linestyle=linestyle[i], marker = markers[1], fillstyle="none",      label =  nns[1]  )
+        ax[i,j].loglog(DeepONet_Data[(j+3)*5+1:(j+3)*5+5, 3, i], DeepONet_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[2], linestyle=linestyle[i], marker = markers[2], fillstyle="none", label =  nns[2]  )
+        if j < 4 || i == 3 || i == 4
+            ax[i,j].loglog(PARA_Data[(j+3)*5+1:(j+3)*5+5, 3, i], PARA_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[3], linestyle=linestyle[i], marker = markers[3], fillstyle="none",     label =  nns[3]  )
+        end
+        ax[i,j].loglog(FNO_Data[(j+3)*5+1:(j+3)*5+5, 3, i], FNO_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[4], linestyle=linestyle[i], marker = markers[4], fillstyle="none",      label =  nns[4]  )
+        
+        
+        @info probs[i] , j, "-th column: ", " solpe : ",    [log.(FNO_Data[(j+3)*5+1:(j+3)*5+5, 3, i])   ones(5)] \ log.(FNO_Data[(j+3)*5+1:(j+3)*5+5, 5, i])
+        
+        # set titles on top row
+        if i == 1
+            ax[i,j].set_title(L"N = "*string(Int(N_Data[j])),pad=20)
+        end
+
+        # set xlabels on bottom row
+        if i == 4
+            ax[i,j].set_xlabel("Evaluation cost",labelpad=2)
+        end
+
+        # # gray spines and ticks 
+        ax[i,j].spines["top"].set_visible(false)
+        ax[i,j].spines["right"].set_visible(false)
+        ax[i,j].spines["left"].set_color("#808080")
+        ax[i,j].spines["left"].set_linewidth(0.3)
+        ax[i,j].spines["bottom"].set_color("#808080")
+        ax[i,j].spines["bottom"].set_linewidth(0.3)
+        ax[i,j][:xaxis][:set_tick_params](colors="#808080",width=0.3)
+        ax[i,j][:xaxis][:set_tick_params](which="minor",bottom=false) # remove minor tick labels
+        ax[i,j][:yaxis][:set_tick_params](colors="#808080",width=0.3)
+        ax[i,j][:yaxis][:set_tick_params](which="minor",left=false)
+    
+                
+        if i == 3
+            ax[i,j].set_yticks([0.05, 0.1, 0.2, 0.4])
+            ax[i,j].set_yticklabels(["0.05","0.1","0.2","0.4"])
+        elseif i == 4
+            ax[i,j].set_yticks([0.1, 0.2, 0.4])
+            ax[i,j].set_yticklabels(["0.1","0.2","0.4"])
+        end
+
+        ax[i,j][:yaxis][:set_minor_formatter](plt.NullFormatter())
+        # set labels on left column and remove tick labels elsewhere
+        if j == 1
+            ax[i,j].set_ylabel(probs[i],labelpad = ypad[i])
+        end
+    end
+end
+
+handles, labels = ax[1,1].get_legend_handles_labels()
+plt.subplots_adjust(bottom=0.07,top=0.9,left=0.13,right=0.98,hspace=0.2)
+
+fig.legend(handles,labels,loc = "upper center",bbox_to_anchor=(0.5,0.95),ncol=4,frameon=false,fontsize=9)
+fig.text(0.01, 0.5, "Test Error", ha="left", va="center", rotation="vertical",fontsize=14)
+plt.savefig("All-Error-Cost.pdf")
+plt.close("all")
---
+++ plot-err-v-cost10k.jl
@@ -0,0 +1,57 @@
+
+
+
+########### Error vs Cost
+fig, ax = PyPlot.subplots(ncols = 4,nrows=1, figsize=(6,2))
+N_Data = [2500, 5000, 10000, 20000]
+ypad = [1, 1, 2,7]
+
+for i = 1:4 # test problems
+    j = 3 # 10k data
+    ax[i].loglog(PCA_Data[(j+3)*5+1:(j+3)*5+5, 3, i], PCA_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[1], linestyle=linestyle[3], marker = markers[1], fillstyle="none",      label =  nns[1]  )
+    ax[i].loglog(DeepONet_Data[(j+3)*5+1:(j+3)*5+5, 3, i], DeepONet_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[2], linestyle=linestyle[3], marker = markers[2], fillstyle="none", label =  nns[2]  )
+    if j < 4 || i == 3 || i == 4
+        ax[i].loglog(PARA_Data[(j+3)*5+1:(j+3)*5+5, 3, i], PARA_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[3], linestyle=linestyle[3], marker = markers[3], fillstyle="none",     label =  nns[3]  )
+    end
+    ax[i].loglog(FNO_Data[(j+3)*5+1:(j+3)*5+5, 3, i], FNO_Data[(j+3)*5+1:(j+3)*5+5, 5, i], color = colors[4], linestyle=linestyle[3], marker = markers[4], fillstyle="none",      label =  nns[4]  )
+        
+        
+    # set titles on top row
+    ax[i].set_title(probs[i],pad=15,fontsize=10)
+
+    # set xlabels on bottom row
+    ax[i].set_xlabel("Evaluation cost",labelpad=2)
+
+    # # gray spines and ticks 
+    ax[i].spines["top"].set_visible(false)
+    ax[i].spines["right"].set_visible(false)
+    ax[i].spines["left"].set_color("#808080")
+    ax[i].spines["left"].set_linewidth(0.3)
+    ax[i].spines["bottom"].set_color("#808080")
+    ax[i].spines["bottom"].set_linewidth(0.3)
+    ax[i][:xaxis][:set_tick_params](colors="#808080",width=0.3)
+    ax[i][:xaxis][:set_tick_params](which="minor",bottom=false) # remove minor tick labels
+    ax[i][:yaxis][:set_tick_params](colors="#808080",width=0.3)
+    ax[i][:yaxis][:set_tick_params](which="minor",left=false)
+    
+                
+    if i == 3
+        ax[i].set_yticks([0.05, 0.1, 0.2, 0.4])
+        ax[i].set_yticklabels(["0.05","0.1","0.2","0.4"])
+    elseif i == 4
+        ax[i].set_yticks([0.1, 0.2, 0.4])
+        ax[i].set_yticklabels(["0.1","0.2","0.4"])
+    end
+
+    ax[i][:yaxis][:set_minor_formatter](plt.NullFormatter())
+
+end
+ax[1].set_ylabel("Test error")
+
+handles, labels = ax[1].get_legend_handles_labels()
+plt.subplots_adjust(bottom=0.2,top=0.8,left=0.09,right=0.99,wspace=0.33)
+
+fig.legend(handles,labels,loc = "upper center",bbox_to_anchor=(0.5,0.93),ncol=4,frameon=false,fontsize=8,handlelength=0)
+# fig.text(0.01, 0.5, "Test Error", ha="left", va="center", rotation="vertical",fontsize=14)
+plt.savefig("tenk-Error-Cost.pdf")
+plt.close("all")
